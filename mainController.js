5e17ea3a9b54effa80b3782ed22eae043d3e5965
amitjha21 <amitjha@usc.edu>
2017-07-20 12:27:08 -0700

    Initial blueprint v0.1

---
+++ mainController.js
@@ -0,0 +1,30 @@
+(function() {
+
+  angular
+    .module('cric')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function MainController(LocalStorage, QueryService) {
+
+    // 'controller as' syntax
+    var self = this;
+
+
+    ////////////  function definitions
+
+
+    /**
+     * Load some data
+     * @return {Object} Returned object
+     */
+    // QueryService.query('GET', 'posts', {}, {})
+    //   .then(function(ovocie) {
+    //     self.ovocie = ovocie.data;
+    //   });
+  }
+
+
+})();
